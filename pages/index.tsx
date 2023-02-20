bad3abe8735952ba98f8ef490bbce153194b2687
Frederick Ankamah Asamoah Jr <fasamoah@wsinnovate.com>
2023-02-20 07:23:59 -0500

    Added the Chakra UI icons package Worked on responsiveness for the text behind the book title Worked on the responsiveness of the Header and added a hamburger menu when screen in below a certain threshold Added a Layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
 import HeadComponent from "@/components/HeadComponent";
 import Header from "@/components/Header";
 import { suranna } from "@/fonts/suranna";
-import { Flex, HStack, Tag, Text, Image, Circle } from "@chakra-ui/react";
+import {
+  Flex,
+  HStack,
+  Tag,
+  Text,
+  Image,
+  Circle,
+  Show,
+  Hide,
+} from "@chakra-ui/react";
 
 /**
  * Home Page
- * 
+ *
  * @returns Home Page component
  */
 export default function Home() {
@@ -22,26 +31,49 @@ export default function Home() {
         <Header />
 
         {/** Book Cover Section */}
-        <Flex py={4} direction={"column"} align={"center"}>
+        <Flex py={4} direction={"column"} align={"center"} width={"100vw"}>
           <Image
             pos={"relative"}
             src={"/Book Cover Template.png"}
             alt={"Book cover placeholder"}
-            height={600}
+            height={{ base: 400, md: 600 }}
             zIndex={1}
-            width={450}
+            width={{ base: 250, md: 450 }}
           />
-          <Text
-            pos={"absolute"}
-            top={350}
-            fontFamily={suranna.style.fontFamily}
-            fontSize={"68px"}
-            textColor={"gray"}
-            zIndex={0}
-            letterSpacing={30}
-          >
-            The Precarious Pivot
-          </Text>
+          <Show above="lg">
+            <Text
+              pos={"absolute"}
+              top={350}
+              textAlign={"center"}
+              fontFamily={suranna.style.fontFamily}
+              fontSize={"68px"}
+              textColor={"gray"}
+              zIndex={0}
+              letterSpacing={"30px"}
+            >
+              The Precarious Pivot
+            </Text>
+          </Show>
+          <Hide above="lg">
+            <Text
+              pos={"absolute"}
+              top={{ base: 40, md: 50 }}
+              textAlign={"center"}
+              fontFamily={suranna.style.fontFamily}
+              fontSize={{ base: "44px", md: "68px" }}
+              textColor={"gray"}
+              zIndex={0}
+              letterSpacing={{base: 0, sm: 16, md: 30 }}
+            >
+              The
+              <br />
+              <br />
+              Precarious
+              <br />
+              <br />
+              Pivot
+            </Text>
+          </Hide>
         </Flex>
 
         {/** Book Title Section */}
@@ -97,49 +129,61 @@ export default function Home() {
         gap={{ base: 4, md: 8, xl: 16 }}
         direction={{ base: "column", md: "row" }}
       >
-        <Flex justify={"center"} align={"center"} direction={"column"} mx={{base: 'auto', md: 0}}>
-          <Image
-            src={"/Book Cover Template.png"}
-            alt={"Book cover placeholder"}
-            height={848}
-            width={590}
-          />
-        </Flex>
+        <Show above="sm">
+          <Flex
+            justify={"center"}
+            align={"center"}
+            direction={"column"}
+            mx={{ base: "auto", md: 0 }}
+          >
+            <Image
+              src={"/Book Cover Template.png"}
+              alt={"Book cover placeholder"}
+              height={848}
+              width={590}
+            />
+          </Flex>
+        </Show>
+
         <Flex
-          align={{base: 'center', md: 'normal'}}
-          textAlign={'start'}
+          align={{ base: "center", md: "normal" }}
+          textAlign={"start"}
           maxW={{ base: "", md: "440px" }}
           direction={"column"}
           px={2}
         >
-          <Text fontSize={48} fontFamily={suranna.style.fontFamily}>
-            The Precarious Pivot
+          <Text
+            fontSize={{ base: 36, sm: 48 }}
+            fontFamily={suranna.style.fontFamily}
+          >
+            Summary
           </Text>
+
           <Text mt={4} fontFamily={suranna.style.fontFamily} fontSize={20}>
             What does it mean to be blessed with a power that when drawn upon
             harms everyone near them?
             <br />
-            <br /> 
-            How does one embrace a pivot towards their
-            destiny when everything they know and love has to be left behind?
-            <br /> 
+            <br />
+            How does one embrace a pivot towards their destiny when everything
+            they know and love has to be left behind?
+            <br />
             <br />
             These are all questions recent college graduate Daniel Ansah finds
             himself answering after he awakens a mysterious power within him,
             injuring numerous people while trying to do what he was trained to
-            do –– be a mental health clinician.  
+            do –– be a mental health clinician. 
             <br />
             <br />
-            Turned on, sold out, and hunted
-            by the same people he swore to help heal, Daniel is given another
-            chance at life when his powers save him and take him to a distant
-            planet named Rathim. Kamuria –– a small village with a large heart
-            –– welcomes Daniel with open arms and contains everything Daniel
-            needs to heal and realize his true destiny. 
+            Turned on, sold out, and hunted by the same people he swore to help
+            heal, Daniel is given another chance at life when his powers save
+            him and take him to a distant planet named Rathim. Kamuria –– a
+            small village with a large heart –– welcomes Daniel with open arms
+            and contains everything Daniel needs to heal and realize his true
+            destiny.
             <br />
             <br />
-            However, with the pain
-            and trauma Daniel has suffered on Earth, he may be beyond saving.
+            However, with the pain and trauma Daniel has suffered on Earth, he
+            may be beyond saving.
           </Text>
         </Flex>
       </Flex>
