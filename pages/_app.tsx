df56100fd29fd6a9e63217a59667b5563f691505
Frederick Ankamah Asamoah Jr <fasamoah@wsinnovate.com>
2023-03-22 10:21:01 -0400

    Adding vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,17 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "@/components/Layout";
 import theme from "@/styles/theme";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+        <Analytics />
+      </>
     </ChakraProvider>
   );
 }
